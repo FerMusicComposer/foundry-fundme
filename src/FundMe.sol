cadedffd8c91c9979d709e6b309e8793b6e0ffff
FerMusicComposer <lfernandosalamanca@gmail.com>
2025-05-16 21:11:51 +0200

    added contracts from previous excersise and installed chainlink brownie-contracts to use AggregatorV3Interface

---
+++ src/FundMe.sol
@@ -0,0 +1,74 @@
+// Get funds from users
+// Withdraw funds
+// Set min funding value
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 5e18;
+    address private immutable i_owner;
+    address[] private s_funders;
+    mapping(address funder => uint256 amountFunded)
+        private s_addressToAmountFunded;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "didn't send enough ETH"
+        );
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+
+        //Different ways of withdrawing from an address:
+        //1. transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        //2. send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "send operation failed");
+
+        //3. call (this is the recommended way of doing it)
+        (bool callSuccess, ) = msg.sender.call{value: address(this).balance}(
+            ""
+        );
+        require(callSuccess, "Call failed");
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}

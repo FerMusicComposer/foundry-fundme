3e56eb48064ab8372ce92ab13c8b0b1ac2bc0ba9
FerMusicComposer <lfernandosalamanca@gmail.com>
2025-05-18 15:49:54 +0200

    added integration tests and script to fund and withdraw from the contract

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployFundMe deployContract = new DeployFundMe();
+        fundMe = deployContract.run();
+        vm.prank(USER);
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testInteractionsUserCanFund() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+        assert(address(fundMe).balance > 0);
+    }
+
+    function testInteractionsUserCanWithdraw() public {
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+        assert(address(fundMe).balance == 0);
+    }
+}
